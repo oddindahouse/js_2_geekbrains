45805e2185d2b41f2ec890a6543f6af23765d60b
oddindahouse <oddindahouse@gmail.com>
2020-05-02 19:46:22 +0500

    updated tree structure 2 lesson base

--- project/js/main.js
+++ project/js/main.js
@@ -1,24 +1,56 @@
 'use strict';
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this._render();
+    }
 
-const renderProduct = (title, price, img = 'https://via.placeholder.com/200x150') => {
-  return `<div class="product-item">
-            <img src="${img}" alt="img">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button class="by-btn">Добавить в корзину</button>
-          </div>`;
-};
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ]
+    }
 
-const renderProducts = list => {
-  list.forEach( good => document.querySelector('.products')
-      .insertAdjacentHTML('beforeend', renderProduct(good.title, good.price))
-  );
-};
+    _render() {
+        const block = document.querySelector(this.container);
 
-renderProducts(products);
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Добавить в корзину</button>
+                </div>
+            </div>`;
+    }
+}
+
+new ProductList();
+
+
+
+
+
