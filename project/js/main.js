193b123e00bdad471af975f4bd7a743ce508f195
oddindahouse <oddindahouse@gmail.com>
2020-05-03 13:55:09 +0500

    Выполнено 1 задание 2 урока

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 class ProductList {
-    constructor(container = '.products') {
+    constructor(cart, container = '.products') {
+        this.cart = cart;
         this.container = container;
         this.goods = [];
         this.allProducts = [];
         this._fetchProducts();
         this._render();
+
     }
 
     _fetchProducts() {
@@ -24,8 +26,10 @@ class ProductList {
             const productObject = new ProductItem(product);
             this.allProducts.push(productObject);
             block.insertAdjacentHTML('beforeend', productObject.render());
+            productObject.addToCartButtonHandler(this.cart);
         }
     }
+
 }
 
 class ProductItem {
@@ -46,9 +50,92 @@ class ProductItem {
                 </div>
             </div>`;
     }
+    addToCartButtonHandler(cart){
+        let _handler = () => {cart.addItem(this)}
+        document.querySelector(`.product-item[data-id="${this.id}"]`).querySelector('.buy-btn')
+            .addEventListener('click', _handler.bind(this) );
+    }
 }
 
-new ProductList();
+class Cart{
+    constructor(container = '.cart__item-list') {
+        this.container = container;
+        this.cartItemList = [];
+        this._addCartButtonHandler();
+
+    }
+    addItem(productItem){
+        let el = this.cartItemList.find(el => el.id === productItem.id);
+        if (el !== undefined) {
+            el.quantity++;
+            document.querySelector(`.cart__item[data-id="${el.id}"] .cart__item-quantity`).textContent = el.quantity;
+
+        } else {
+            let newCartItem =  this._product2CartItem(productItem);
+            document.querySelector(this.container).insertAdjacentHTML('beforeend', newCartItem.render());
+            newCartItem.addDelBtnHandler(this, this.container);
+            this.cartItemList.push(newCartItem);
+
+        }
+
+    }
+    _addCartButtonHandler(){
+        document.querySelector('.cart__btn').addEventListener('click', () => {
+            let isVisible = (vElement) => {
+                return !(vElement.offsetWidth == 0 && vElement.offsetHeight == 0)
+                    && window.getComputedStyle(vElement).visibility != "hidden";
+            }
+
+            if (isVisible(document.querySelector('.cart__drop'))){
+                document.querySelector('.cart__drop').style.visibility = 'hidden';
+            } else
+            {
+                document.querySelector('.cart__drop').style.visibility = 'visible';
+            }
+
+        })
+    }
+    _product2CartItem(productItem){
+        return new CartItem(productItem);
+    }
+
+}
+
+class CartItem{
+    constructor(product, img = 'https://placehold.it/60x60') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+        this.quantity = 1;
+    }
+    render() {
+        return `<li class="cart__item" data-id="${this.id}">
+                <img class="cart__item-img" src="${this.img}" alt="Some img">
+                    <p class="cart__item-title">${this.title}</p>
+                    <p class="cart__item-price">${this.price} \u20bd</p>
+                    <p class="cart__item-quantity">${this.quantity}</p>
+                    <i class="fas fa-times-circle cart__item-delbtn"></i>
+            </li>`;
+    }
+    _changeQuantity(cart, container){
+        let el = document.querySelector(`.cart__item[data-id="${this.id}"]`)
+        if (this.quantity > 1){
+            this.quantity--;
+            el.querySelector('.cart__item-quantity').textContent = this.quantity;
+        } else{
+            cart.cartItemList.splice(cart.cartItemList.findIndex(el => el === this));
+            document.querySelector(container).removeChild(el);
+        }
+    }
+
+    addDelBtnHandler(cart, container){
+        document.querySelector(`.cart__item[data-id="${this.id}"] .cart__item-delbtn`)
+            .addEventListener('click',this._changeQuantity.bind(this, cart, container));
+    }
+}
+const cart = new Cart();
+new ProductList(cart);
 
 
 
