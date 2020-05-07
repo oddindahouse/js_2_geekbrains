006486ada14a57780b29de43566941409ed4aede
oddindahouse <oddindahouse@gmail.com>
2020-05-07 19:54:02 +0500

    3 ДЗ

--- project/js/main.js
+++ project/js/main.js
@@ -29,11 +29,11 @@ class ProductList {
         // this._fetchProducts();
         this._getProducts()
             .then(data => {
-                console.log(data);
                 this.goods = [...data];
                 this._render();
+                console.log(this._calculateTotalProductsSum());
             });
-        console.log(this._calculateTotalProductsSum())
+
     }
 
     _getProducts() {
@@ -68,19 +68,32 @@ class ProductList {
     }
 }
 
-class ProductItem {
-    constructor(product, img = 'https://placehold.it/200x150') {
-        this.title = product.product_name;
-        this.price = product.price;
-        this.id = product.id_product;
+class Item {
+    constructor(product_name, price, id_product, img) {
+        this.product_name = product_name;
+        this.price = price;
+        this.id_product = id_product;
         this.img = img;
+    }
+}
+
+class ProductItem extends Item {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        super(
+            product.product_name,
+            product.price,
+            +product.id_product,
+            img
+        );
+
+
     }
 
     render() {
-        return `<div class="product-item" data-id="${this.id}">
+        return `<div class="product-item" data-id="${this.id_product}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
-                    <h3>${this.title}</h3>
+                    <h3>${this.product_name}</h3>
                     <p>${this.price} \u20bd</p>
                     <button class="buy-btn">Добавить в корзину</button>
                 </div>
@@ -91,40 +104,94 @@ class ProductItem {
         let _handler = () => {
             cart.addItem(this)
         }
-        document.querySelector(`.product-item[data-id="${this.id}"]`).querySelector('.buy-btn')
+        document.querySelector(`.product-item[data-id="${this.id_product}"]`).querySelector('.buy-btn')
             .addEventListener('click', _handler.bind(this));
     }
 }
 
 class Cart {
     constructor(container = '.cart__item-list') {
-        this.container = container;
+        this.amount = 0;
+        this.countGoods = null;
         this.cartItemList = [];
+        this.container = container;
+        this._getCartItems().then(() => {
+            this._render();
+        });
         this._addCartButtonHandler();
 
     }
 
+
+    _getCartItems() {
+        return fetch(`${API}/getBasket.json`)
+            .then(response => response.json()).then(data => {
+                this.amount = data.amount;
+                this.countGoods = data.countGoods;
+                this.cartItemList = [...data.contents].map(product => this._product2CartItem(product));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    _render() {
+        for (let item of this.cartItemList) {
+            document.querySelector(this.container).insertAdjacentHTML('beforeend', item.render());
+            //навешиваем обработчик события на кнопку удаления товара
+            item.addDelBtnHandler(this, this.container);
+        }
+    }
+
+    _fetchCartItemAdd(productItem) {
+        fetch(`${API}/addToBasket.json`, {
+            method: 'POST',
+            body: productItem
+        })
+            .then(response => {
+                if (!response.ok) {
+                    // Сервер вернул код ответа за границами диапазона [200, 299]
+                    return Promise.reject(new Error(
+                        'Response failed: ' + response.status + ' (' + response.statusText + ')'
+                    ));
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (data.result !== 1) {
+                    return Promise.reject(new Error('Товар не был добавлен в корзину'));
+                }
+            }).catch(error => {
+            console.log(error);
+        });
+    }
+
     addItem(productItem) {
-        let el = this.cartItemList.find(el => el.id === productItem.id);
+        //отправляем добавленный товар на сервер
+        this._fetchCartItemAdd(productItem);
+        //работаем с локальной копией товаров
+        let el = this.cartItemList.find(el => el.id_product === productItem.id_product);
         if (el !== undefined) {
             el.quantity++;
-            document.querySelector(`.cart__item[data-id="${el.id}"] .cart__item-quantity`).textContent = el.quantity;
+            document.querySelector(`.cart__item[data-id="${el.id_product}"] .cart__item-quantity`).textContent = el.quantity;
 
         } else {
             let newCartItem = this._product2CartItem(productItem);
             document.querySelector(this.container).insertAdjacentHTML('beforeend', newCartItem.render());
+            //навешиваем обработчик события на кнопку удаления товара
             newCartItem.addDelBtnHandler(this, this.container);
             this.cartItemList.push(newCartItem);
 
         }
+
         this.refreshTotalPrice();
     }
 
     _addCartButtonHandler() {
         document.querySelector('.cart__btn').addEventListener('click', () => {
             let isVisible = (vElement) => {
-                return !(vElement.offsetWidth == 0 && vElement.offsetHeight == 0)
-                    && window.getComputedStyle(vElement).visibility != "hidden";
+                return !(vElement.offsetWidth === 0 && vElement.offsetHeight === 0)
+                    && window.getComputedStyle(vElement).visibility !== "hidden";
             }
 
             if (isVisible(document.querySelector('.cart__drop'))) {
@@ -150,27 +217,53 @@ class Cart {
 
 }
 
-class CartItem {
+class CartItem extends Item{
     constructor(product, img = 'https://placehold.it/60x60') {
-        this.title = product.title;
-        this.price = product.price;
-        this.id = product.id;
-        this.img = img;
+        super(
+            product.product_name,
+            product.price,
+            product.id_product,
+            img
+        );
         this.quantity = 1;
     }
 
     render() {
-        return `<li class="cart__item" data-id="${this.id}">
+        return `<li class="cart__item" data-id="${this.id_product}">
                 <img class="cart__item-img" src="${this.img}" alt="Some img">
-                    <p class="cart__item-title">${this.title}</p>
+                    <p class="cart__item-title">${this.product_name}</p>
                     <p class="cart__item-price">${this.price} \u20bd</p>
                     <p class="cart__item-quantity">${this.quantity}</p>
                     <i class="fas fa-times-circle cart__item-delbtn"></i>
             </li>`;
     }
 
-    _changeQuantity(cart, container) {
-        let el = document.querySelector(`.cart__item[data-id="${this.id}"]`)
+    _fetchCartItemDel(item) {
+        fetch(`${API}/deleteFromBasket.json`, {
+            method: 'POST',
+            body: item
+        })
+            .then(response => {
+                if (!response.ok) {
+                    // Сервер вернул код ответа за границами диапазона [200, 299]
+                    return Promise.reject(new Error(
+                        'Response failed: ' + response.status + ' (' + response.statusText + ')'
+                    ));
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (data.result !== 1) {
+                    return Promise.reject(new Error('Товар не был удален из корзины'));
+                }
+            }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    _delItemFromCart(cart, container) {
+        this._fetchCartItemDel(new Item(this.product_name, this.price, this.id_product, this.img));
+        let el = document.querySelector(`.cart__item[data-id="${this.id_product}"]`)
         if (this.quantity > 1) {
             this.quantity--;
             el.querySelector('.cart__item-quantity').textContent = this.quantity;
@@ -182,8 +275,8 @@ class CartItem {
     }
 
     addDelBtnHandler(cart, container) {
-        document.querySelector(`.cart__item[data-id="${this.id}"] .cart__item-delbtn`)
-            .addEventListener('click', this._changeQuantity.bind(this, cart, container));
+        document.querySelector(`.cart__item[data-id="${this.id_product}"] .cart__item-delbtn`)
+            .addEventListener('click', this._delItemFromCart.bind(this, cart, container));
     }
 }
 
